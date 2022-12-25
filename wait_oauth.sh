cbe15044e06dcb606b4c0112ff0897ec280b1c9e
Zhu Cheng <zhucheng@ocpsvc.zch.org>
2022-12-26 04:04:43 +0800

    ajust timeout

--- wait_oauth.sh
+++ wait_oauth.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
   
 oc wait co/authentication --for condition=Progressing \
-    --timeout 90s
+    --timeout 190s
 oc rollout status -n openshift-authentication deployment/oauth-openshift \
-    --timeout 90s
+    --timeout 190s
 while [ -n "$(oc get pod -n openshift-authentication -o \
 jsonpath='{.items[?(@.metadata.deletionTimestamp != "")].metadata.name}')" ]
 do
-    sleep 3
+    sleep 30
 done
 
