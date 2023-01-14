c5ce244b87a90228130099fcb390d5e732bf0106
zhuchengsg <106645697+zhuchengsg@users.noreply.github.com>
2023-01-15 00:48:29 +0800

    Update wait_oauth.sh

--- wait_oauth.sh
+++ wait_oauth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
   
-oc wait co/authentication --for condition=Progressing \
-    --timeout 190s
+# oc wait co/authentication --for condition=Progressing \
+#    --timeout 190s
 oc rollout status -n openshift-authentication deployment/oauth-openshift \
     --timeout 190s
 while [ -n "$(oc get pod -n openshift-authentication -o \
